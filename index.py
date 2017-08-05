ccf6fff32c83d05e29f880fb0de2cf5c56ea941d
Ben Specht <s3604514@student.rmit.edu.au>
2017-08-06 06:22:32 +1000

    no message

--- index.py
+++ index.py
@@ -14,13 +14,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def get():
+    # fix random seed for reproducibility
+    seed = 7
+    numpy.random.seed(seed)
+
     # load dataset
     dataframe = pandas.read_csv("file.csv", header=None)
     dataset = dataframe.values
-
     # split into input (X) and output (Y) variables
-    X = dataset[:, 0:3].astype(float)
-    Y = dataset[:, 3]
+    X = dataset[:, 0:8].astype(float)
+    Y = dataset[:, 8]
 
     # encode class values as integers
     encoder = LabelEncoder()
@@ -28,14 +31,13 @@ def get():
     encoded_Y = encoder.transform(Y)
 
     model = Sequential()
-    model.add(Dense(6, input_dim=8, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(16, input_dim=8, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))  # replace with relu
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    model.fit(X, encoded_Y, epochs=100, batch_size=1)
-
-    return json.dumps(model.predict(numpy.array([[0.01, 0.05, 0.01]])))
+    model.fit(X, encoded_Y, epochs=200, batch_size=1)
+    return "{\nSuccess\n}"
 
 
 @app.route('/post')
@@ -51,33 +53,15 @@ def post():
 
 
 def run_ml_on_json(data_json):
-    # data = json.loads(data_json)
-    #
-    # with open("data.csv", "w") as file:
-    #     csv_file = csv.writer(file)
-    #     csv_file.writerow(["Emotion", "Score", "classification"])
-    #     for item in data:
-    #         csv_file.writerow(["Anger", item['scores']["anger"], item['classification']])
+    data = json.loads(data_json)
 
-    # # load dataset
-    # dataframe = pandas.read_csv("file.csv", header=None)
-    # dataset = dataframe.values
-    #
-    # # split into input (X) and output (Y) variables
-    # X = dataset[:, 0:3].astype(float)
-    # Y = dataset[:, 3]
-    #
-    # # encode class values as integers
-    # encoder = LabelEncoder()
-    # encoder.fit(Y)
-    # encoded_Y = encoder.transform(Y)
-    #
-    # model = Sequential()
-    # model.add(Dense(6, input_dim=8, kernel_initializer='normal', activation='relu'))
-    # model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))  # replace with relu
-    # # Compile model
-    # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    #
-    # model.fit(X, encoded_Y, epochs=100, batch_size=1)
-    #
-    # return json.dumps(model.predict(numpy.array([[0.01, 0.05, 0.01]])))
+    anger = data.scores.anger
+    contempt = data.scores.contempt
+    disgust = data.scores.disgust
+    fear = data.scores.fear
+    happiness = data.scores.happiness
+    neutral = data.scores.neutral
+    sadness = data.scores.sadness
+    surprise = data.scores.surprise
+
+    return json.dumps(model.predict(numpy.array([[anger, contempt, disgust, fear, happiness, neutral, sadness, surprise]])))
