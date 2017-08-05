a4ac57962ab4851114334c5773d892c0c979b718
Ben Specht <s3604514@student.rmit.edu.au>
2017-08-06 05:41:47 +1000

    no message

--- index.py
+++ index.py
@@ -14,7 +14,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def get():
-    return "hello world"
+    # load dataset
+    dataframe = pandas.read_csv("file.csv", header=None)
+    dataset = dataframe.values
+
+    # split into input (X) and output (Y) variables
+    X = dataset[:, 0:3].astype(float)
+    Y = dataset[:, 3]
+
+    # encode class values as integers
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    encoded_Y = encoder.transform(Y)
+
+    model = Sequential()
+    model.add(Dense(6, input_dim=8, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))  # replace with relu
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(X, encoded_Y, epochs=100, batch_size=1)
+
+    return json.dumps(model.predict(numpy.array([[0.01, 0.05, 0.01]])))
 
 
 @app.route('/post')
@@ -38,25 +59,25 @@ def run_ml_on_json(data_json):
     #     for item in data:
     #         csv_file.writerow(["Anger", item['scores']["anger"], item['classification']])
 
-    # load dataset
-    dataframe = pandas.read_csv("file.csv", header=None)
-    dataset = dataframe.values
-
-    # split into input (X) and output (Y) variables
-    X = dataset[:, 0:3].astype(float)
-    Y = dataset[:, 3]
-
-    # encode class values as integers
-    encoder = LabelEncoder()
-    encoder.fit(Y)
-    encoded_Y = encoder.transform(Y)
-
-    model = Sequential()
-    model.add(Dense(6, input_dim=8, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))  # replace with relu
-    # Compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    model.fit(X, encoded_Y, epochs=100, batch_size=1)
-
-    return json.dumps(model.predict(numpy.array([[0.01, 0.05, 0.01]])))
+    # # load dataset
+    # dataframe = pandas.read_csv("file.csv", header=None)
+    # dataset = dataframe.values
+    #
+    # # split into input (X) and output (Y) variables
+    # X = dataset[:, 0:3].astype(float)
+    # Y = dataset[:, 3]
+    #
+    # # encode class values as integers
+    # encoder = LabelEncoder()
+    # encoder.fit(Y)
+    # encoded_Y = encoder.transform(Y)
+    #
+    # model = Sequential()
+    # model.add(Dense(6, input_dim=8, kernel_initializer='normal', activation='relu'))
+    # model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))  # replace with relu
+    # # Compile model
+    # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #
+    # model.fit(X, encoded_Y, epochs=100, batch_size=1)
+    #
+    # return json.dumps(model.predict(numpy.array([[0.01, 0.05, 0.01]])))
